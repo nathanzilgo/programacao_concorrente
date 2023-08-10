b19be44cb97bfafc6dec455aee23d3e9c9054bb8
Nathan <nathan.pedroza@inda.band>
2023-08-10 17:22:52 -0300

    update_hashmap

--- hashmap.py
+++ hashmap.py
@@ -2,10 +2,11 @@ import threading
 
 
 class HashMap:
-    def __init__(self):
-        self.size = 0
-        self.capacity = 150
-        self.map = [None] * self.capacity
+    def __init__(self, size=0, capacity=150):
+        self.size = size
+        self.capacity = capacity
+        self.bucket = [None] * self.capacity
+        self.locks = [threading.Lock() for _ in range(self.capacity)]  # Um lock por bucket
 
     def hash(self, key):
         hash = 0
@@ -14,27 +15,33 @@ class HashMap:
         return hash % self.capacity
 
     def put(self, key, value):
-        hash = self.hash(key)
-        lock = threading.Lock(1)
-        lock.acquire()
-        print(f'Hash: {hash}')
-        if self.map[hash] is None:
-            self.map[hash] = [key, value]
-            self.size += 1
-        else:
-            self.map[hash][1] = value
-            self.size += 1
-        lock.release()
+        index = self._hash(key)
+        with self.locks[index]:
+            if self.bucket_array[index] is None:
+                self.bucket_array[index] = []
+            self.bucket_array[index].append((key, value))
 
     def get(self, key):
-        hash = self.hash(key)
-        if self.map[hash] is None:
-            return None
-        else:
-            return self.map[hash][1]
+        index = self._hash(key)
+        with self.locks[index]:
+            if self.bucket_array[index] is not None:
+                for stored_key, value in self.bucket_array[index]:
+                    if stored_key == key:
+                        return value
+        return None
+
+    def remove(self, key):
+        index = self._hash(key)
+        with self.locks[index]:
+            if self.bucket_array[index] is not None:
+                for i, (stored_key, _) in enumerate(self.bucket_array[index]):
+                    if stored_key == key:
+                        self.bucket_array[index].pop(i)
+                        return
+        raise KeyError(f"Key '{key}' not found.")
 
     def print(self):
-        print(list(filter(lambda x: x is not None, self.map)))
+        print(list(filter(lambda x: x is not None, self.bucket)))
 
 
 hm = HashMap()
