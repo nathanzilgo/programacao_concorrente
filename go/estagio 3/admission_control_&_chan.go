26a662001361fc3c046b9a895f5a61539ea4b070
Nathan <nathan.pedroza@inda.band>
2023-10-18 22:56:06 -0300

    admission control

--- go/estagio 3/admission_control_&_chan.go
+++ go/estagio 3/admission_control_&_chan.go
@@ -1,16 +1,44 @@
 package main
 
-import "fmt"
+
+import (
+	"fmt"
+	"time"
+)
 
 const maxCapacity = 10
 
-func create_req()
+type Request struct {
+	ID int
+}
 
-func exec_req()
+func create_req() Request {
+	return Request{ID: time.Now().Nanosecond()}
+}
+
+func exec_req(req Request) {
+	// Simular o processamento demorado
+	time.Sleep(2 * time.Second)
+	fmt.Printf("Requisição %d processada\n", req.ID)
+}
 
 func main() {
+	reqChan := make(chan Request, maxCapacity)
+	doneCh := make(chan Request)
+	
+	go func () {
+		for req := range reqChan {
+			exec_req(req)
+			doneCh <- req
+		}
+	} ()
 
 	for {
-		var req = create_req()
+		select {
+			case reqChan <- create_req():
+				fmt.Printf("Requisição criada\n")
+			case req := <- doneCh:
+				fmt.Printf("Requisicao %d processada\n", req.ID)
+		}
 	}
 }
